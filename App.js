418c537707215a324d9504a10bd29fe27884cd79
Satish <satish.appanasamy@gmail.com>
2023-01-30 12:45:59 +1100

    feat -created App.js

--- App.js
+++ App.js
@@ -1,11 +1,57 @@
+import React, {useEffect, useState} from 'react';
+import {ActivityIndicator, StyleSheet, FlatList, Text, View} from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StyleSheet, Text, View } from 'react-native';
+import SearchBar from "./SearchBar";
+import List from "./List";
 
 export default function App() {
+  const apiKey = '05d95baf9e3a430c9d4afdf68efb75d9'
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+  const [searchPhrase, setSearchPhrase] = useState("");
+  const [clicked, setClicked] = useState(false);
+
+  const getMovies = async () => {
+    try {
+      const response = await fetch(`https://newsapi.org/v2/everything?apiKey=${apiKey}&q=${searchPhrase}`);
+      const json = await response.json();
+      setData(json.articles);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  // const handleSearchPhraseChange = event => {
+  //   setSearchPhrase(event.target.value);
+  //   useCallback(
+  //     debounce(getMovies, 200),
+  //     []
+  //   );
+  // };
+
+  useEffect(() => {
+    getMovies();
+  }, [searchPhrase]);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.main}>
+      <SearchBar
+        searchPhrase={searchPhrase}
+        setSearchPhrase={setSearchPhrase}
+        clicked={clicked}
+        setClicked={setClicked}
+      />
+      {isLoading ? (
+        <ActivityIndicator />
+      ) : (<List
+        searchPhrase={searchPhrase}
+        data={data}
+        setClicked={setClicked}
+      />
+      )}
     </View>
   );
 }
@@ -16,5 +62,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  main: {flex: 1, padding: 24},
+  item: {
+    margin: 0,
+    borderBottomWidth: 2,
+    borderBottomColor: "lightgrey"
   },
 });
---
+++ List.js
@@ -0,0 +1,59 @@
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  SafeAreaView,
+} from "react-native";
+
+// definition of the Item, which will be rendered in the FlatList
+const Item = ({ title, description }) => (
+  <View style={styles.item}>
+    <Text style={styles.title}>{title}</Text>
+    <Text style={styles.description}>{description}</Text>
+  </View>
+);
+
+const List = ({ searchPhrase, setClicked, data }) => {
+  const renderItem = ({ item }) => {
+    return <Item title={item.title} description={item.description} />;
+  };
+
+  return (
+    <SafeAreaView style={styles.list__container}>
+      <View
+        onStartShouldSetResponder={() => {
+          setClicked(false);
+        }}
+      >
+       <FlatList
+          data={data}
+          keyExtractor={({url}) => url}
+          renderItem={renderItem}
+        />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default List;
+
+const styles = StyleSheet.create({
+  list__container: {
+    margin: 10,
+    height: "85%",
+    width: "100%",
+  },
+  item: {
+    margin: 40,
+    borderBottomWidth: 2,
+    borderBottomColor: "lightgrey"
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 5,
+    fontStyle: "italic",
+  },
+});
